f9dd526f39cab2fd5f0d81ebdcb306d570f1393e
is-hoku <me@hokuishi.be>
2023-01-05 01:37:06 +0900

    add VM and interpreter

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -1,6 +1,7 @@
 package codegen
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/is-hoku/pl0dash-go/getsource"
@@ -109,4 +110,121 @@ func checkMax(fptex *os.File) {
 // 命令語のバックパッチ (次の番地を)
 func BackPatch(i int) {
 	code[i].u.value = cIndex + 1
+}
+
+// 目的コード (命令語) の実行
+func Execute(fptex *os.File) {
+	var stack [MAXMEM]int     // 実行時スタック
+	var display [MAXLEVEL]int // 現在見える各ブロックの先頭番地のディスプレイ
+	var pc, top, lev int
+	var i inst // 実行する命令語
+	fmt.Println("start execution")
+	top = 0        // 次にスタックに入れる場所
+	pc = 0         // 命令語のカウンタ
+	stack[0] = 0   // stack[top] は callee で壊すディスプレイの退避場所
+	stack[1] = 0   // stack[top+1] は caller への戻り番地
+	display[0] = 0 // 主ブロックの先頭番地は 0
+	for {
+		i = code[pc] // これから実行する命令語
+		pc++
+		switch i.opCode {
+		case Lit:
+			stack[top] = i.u.value
+			top++
+		case Lod:
+			stack[top] = stack[display[i.u.addr.Level]+i.u.addr.Addr]
+		case Sto:
+			top--
+			stack[display[i.u.addr.Level]+i.u.addr.Addr] = stack[top]
+		case Cal:
+			// i.u.addr.Level は callee の名前のレベル、 callee のブロックのレベル lev はそれに +1 したもの
+			lev = i.u.addr.Level + 1
+			stack[top] = display[lev] // display[lev] の退避
+			stack[top+1] = pc
+			display[lev] = top // 現在の top が callee のブロックの先頭番地
+			pc = i.u.addr.Addr
+		case Ret:
+			top--
+			temp := stack[top]                   // スタックのトップにあるものが返す値
+			top = display[i.u.addr.Level]        // top を呼ばれたときの値に戻す
+			display[i.u.addr.Level] = stack[top] // 壊したディスプレイの回復
+			pc = stack[top+1]
+			top -= i.u.addr.Addr // 実引数の分だけ top を戻す
+			stack[top] = temp    // 返す値をスタックの top へ
+			top++
+		case Ict:
+			top += i.u.value
+			if top >= MAXMEM-MAXREG {
+				getsource.ErrorF("stack overflow", fptex)
+			}
+		case Jmp:
+			pc = i.u.value
+		case Opr:
+			switch i.u.optr {
+			case Neg:
+				stack[top-1] = -stack[top-1]
+				continue
+			case Add:
+				top--
+				stack[top-1] += stack[top]
+				continue
+			case Sub:
+				top--
+				stack[top-1] -= stack[top]
+				continue
+			case Mul:
+				top--
+				stack[top-1] *= stack[top]
+				continue
+			case Div:
+				top--
+				stack[top-1] /= stack[top]
+				continue
+			case Odd:
+				stack[top-1] = stack[top-1] & 1 // 奇数判定のための論理積演算
+				continue
+			case Eq:
+				top--
+				stack[top-1] = bool2int(stack[top-1] == stack[top])
+				continue
+			case Ls:
+				top--
+				stack[top-1] = bool2int(stack[top-1] < stack[top])
+				continue
+			case Gr:
+				top--
+				stack[top-1] = bool2int(stack[top-1] > stack[top])
+				continue
+			case Neq:
+				top--
+				stack[top-1] = bool2int(stack[top-1] != stack[top])
+				continue
+			case Lseq:
+				top--
+				stack[top-1] = bool2int(stack[top-1] <= stack[top])
+				continue
+			case Greq:
+				top--
+				stack[top-1] = bool2int(stack[top-1] >= stack[top])
+				continue
+			case Wrt:
+				top--
+				fmt.Printf("%d ", stack[top])
+				continue
+			case Wrl:
+				fmt.Println("")
+				continue
+			}
+		}
+		if pc == 0 {
+			break
+		}
+	}
+}
+
+func bool2int(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
 }
--- main.go
+++ main.go
@@ -1,30 +1,36 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
+	"github.com/is-hoku/pl0dash-go/codegen"
 	"github.com/is-hoku/pl0dash-go/compile"
 	"github.com/is-hoku/pl0dash-go/getsource"
 )
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Errorf("invalid argument length")
+		err := errors.New("invalid argument length")
+		fmt.Println(err)
 		return
 	}
 	fileName := os.Args[1]
 	f, tex, scanner, err := getsource.OpenSource(fileName)
 	if err != nil {
-		fmt.Errorf("cannot open the file: %s", err)
+		err := errors.New(fmt.Sprintf("cannot open the file: %s", err))
+		fmt.Println(err)
 		return
 	}
 	defer tex.Close()
 	defer f.Close()
 	if err := compile.Compile(f, scanner); err != nil {
-		err = codegen.Execute(f)
+		codegen.Execute(tex)
 	}
 	if err != nil {
-		fmt.Errorf("Error: %s", err)
+		err := errors.New(fmt.Sprintf("Error: %s", err))
+		fmt.Println(err)
+		return
 	}
 }
